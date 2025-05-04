88d7bd18c958ef0399fa84186c15ee79f8b84cfa
MrRaptorious <35742529+MrRaptorious@users.noreply.github.com>
2025-05-04 12:36:08 +0200

    run services on mysql (invoice broken)

--- spring-petclinic-customers-service/src/main/resources/db/mysql/schema.sql
+++ spring-petclinic-customers-service/src/main/resources/db/mysql/schema.sql
@@ -1,7 +1,8 @@
 CREATE DATABASE IF NOT EXISTS petclinic;
-GRANT ALL PRIVILEGES ON petclinic.* TO pc@localhost IDENTIFIED BY 'pc';
 
-USE petclinic;
+CREATE USER IF NOT EXISTS 'pc'@'localhost' IDENTIFIED BY 'pc';
+GRANT ALL PRIVILEGES ON petclinic.* TO 'pc'@'localhost';
+
 
 CREATE TABLE IF NOT EXISTS types (
   id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
--- spring-petclinic-visits-service/src/main/resources/db/mysql/schema.sql
+++ spring-petclinic-visits-service/src/main/resources/db/mysql/schema.sql
@@ -1,12 +1,11 @@
 CREATE DATABASE IF NOT EXISTS petclinic;
-GRANT ALL PRIVILEGES ON petclinic.* TO pc@localhost IDENTIFIED BY 'pc';
 
-USE petclinic;
+CREATE USER IF NOT EXISTS 'pc'@'localhost' IDENTIFIED BY 'pc';
+GRANT ALL PRIVILEGES ON petclinic.* TO 'pc'@'localhost';
 
 CREATE TABLE IF NOT EXISTS visits (
   id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
   pet_id INT(4) UNSIGNED NOT NULL,
   visit_date DATE,
-  description VARCHAR(8192),
-  FOREIGN KEY (pet_id) REFERENCES pets(id)
+  description VARCHAR(8192)
 ) engine=InnoDB;
